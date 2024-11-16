781fdb9682120bb2275b26d7e57ebcf40ba84549
Tomasz Walecki <tomek_w@outlook.com>
2024-11-16 17:45:20 +0100

    Upgrade to .net9, update nuget packages

--- ExampleConsoleApp/Program.cs
+++ ExampleConsoleApp/Program.cs
@@ -8,24 +8,31 @@ var streamSelector = new StreamSelector();
 var url = "https://www.youtube.com/watch?v=gNn9NxZH2Vo";
 
 Console.WriteLine("Get streams with playable urls");
-// get all streams with 
-var streams = await youTubeStreams.GetAllStreamsAsync(url, true);
-Console.WriteLine($"Found {streams.Count()} streams");
+try
+{
+	// get all streams with 
+	var streams = await youTubeStreams.GetAllStreamsAsync(url, true);
+	Console.WriteLine($"Found {streams.Count()} streams");
 
-var audio = streamSelector.SelectBestAudio(streams);
-Console.WriteLine($"Found audio with best quality {audio.ITag} {audio.Container} {audio.Bitrate}");
-Console.WriteLine($"{audio.PlayableUrl.Url}");
+	var audio = streamSelector.SelectBestAudio(streams);
+	Console.WriteLine($"Found audio with best quality {audio.ITag} {audio.Container} {audio.Bitrate}");
+	Console.WriteLine($"{audio.PlayableUrl.Url}");
 
 
-Console.WriteLine("Get streams wihout playable urls");
-var streams2 = await youTubeStreams.GetAllStreamsAsync(url, false);
-Console.WriteLine($"Found {streams2.Count()} streams");
+	Console.WriteLine("Get streams wihout playable urls");
+	var streams2 = await youTubeStreams.GetAllStreamsAsync(url, false);
+	Console.WriteLine($"Found {streams2.Count()} streams");
 
-var audio2 = streamSelector.SelectBestAudio(streams2);
-Console.WriteLine($"Audio with best quality {audio2.ITag} {audio2.Container} {audio2.Bitrate}");
-Console.WriteLine($"{audio2.PlayableUrl.Url}");
-await audio2.PlayableUrl.PrepareAsync(audio2.RawUrl, youTubeStreams.Decryptor);
-Console.WriteLine($"Audio with best quality {audio2.ITag} {audio2.Container} {audio2.Bitrate}");
-Console.WriteLine($"{audio2.PlayableUrl.Url}");
+	var audio2 = streamSelector.SelectBestAudio(streams2);
+	Console.WriteLine($"Audio with best quality {audio2.ITag} {audio2.Container} {audio2.Bitrate}");
+	Console.WriteLine($"{audio2.PlayableUrl.Url}");
+	await audio2.PlayableUrl.PrepareAsync(audio2.RawUrl, youTubeStreams.Decryptor);
+	Console.WriteLine($"Audio with best quality {audio2.ITag} {audio2.Container} {audio2.Bitrate}");
+	Console.WriteLine($"{audio2.PlayableUrl.Url}");
+}
+catch (Exception ex)
+{
+
+}
 
 Console.WriteLine("Done");
